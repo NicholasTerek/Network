ba2efc91b79e3af804f15f7e7042caa331a97375
Nicholas Terek <NicholasTerek1@gmail.com>
2024-10-14 00:36:15 -0400

    Add initial Value class with redefined operators (+, -, *, /) again

--- src/Value.cpp
+++ src/Value.cpp
@@ -2,3 +2,37 @@
 #include <isostream>
 #include <cmath>
 #include <cassert>
+
+Value::Value(double data, std::vector<Value*> children, std::string op)
+    : data(data), grad(0.0), _op(op), _backward([](){}), _prev(children.begin(), children.end()) {}
+
+
+Value Value::operator+(const Value& other){
+    Value* other_ptr = new Value(other);
+    Value* out = new Value(this->data + other_ptr->data, {this, other_ptr}, "+");
+
+    return *out;
+}
+
+Value Value::operator-(const Value& other){
+    Value* other_ptr = new Value(other);
+    Value* out = new Value(this->data - other_ptr->data, {this, other_ptr}, "-");
+
+    return *out;
+}
+
+Value Value::operator*(const Value& other){
+    Value* other_ptr = new Value(other);
+    Value* out = new Value(this->data * other_ptr->data, {this, other_ptr}, "*")
+
+    return *out;
+}
+
+Value Value::operator/(const Value& other){
+    Value* other_ptr = new Value(other);
+    Value out = new Value(this-> data / other_ptr->data, {this, other_ptr}, "/")
+
+    return *out;
+}
+
+
